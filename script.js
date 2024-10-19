90e69e42bc5187c1ef66037146b8c508afd72731
bharat705 <73097406+bharat705@users.noreply.github.com>
2024-10-19 22:40:41 +0545

    add winning game logic Changes to be committed: 	modified:   script.js

--- script.js
+++ script.js
@@ -1,26 +1,65 @@
 const X_CLASS = "x";
 const O_CLASS = "o";
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 4, 5],
+  [2, 4, 6],
+  [0, 4, 8],
+  [6, 7, 8],
+];
 
 const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.getElementById("game-board");
 let xTurn;
-cellElements.forEach((cell) => {
-  cell.addEventListener("click", handleClick, { once: true });
-});
+
+startGame();
+
+function startGame() {
+  xTurn = true;
+  cellElements.forEach((cell) => {
+    cell.addEventListener("click", handleClick, { once: true });
+  });
+  setBoardHoverClass();
+}
 
 function handleClick(event) {
   const cell = event.target;
   const currentClass = xTurn ? X_CLASS : O_CLASS;
   placeMark(cell, currentClass);
+  if (checkWin(currentClass)) {
+    console.log(`winner ${currentClass}`);
+  }
   // Check for win
   // Check for draw
-  // Switch turns
   switchTurns();
+  setBoardHoverClass();
 }
 
 function placeMark(cell, currentClass) {
   cell.classList.add(currentClass);
 }
 
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
+}
+
 function switchTurns() {
   xTurn = !xTurn;
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS);
+  board.classList.remove(O_CLASS);
+  if (xTurn) {
+    board.classList.add(X_CLASS);
+  } else {
+    board.classList.add(O_CLASS);
+  }
 }
